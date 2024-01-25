330dbc07d1ba24bda9674eed465ee195e07292c1
Nicholas Armstrong <narmstro@warren.k12.in.us>
2024-01-25 06:50:25 -0500

    updates

---
+++ Unit 01_JavaScript_Basics and DOM Manipulation/05_Data Types and Functions/script.js
@@ -0,0 +1,8 @@
+console.log([] + {})
+console.log({} + []);
+console.log("5"  -2 );
+console.log("5" * 2)
+console.log(1 === true);
+console.log(1 == true);
+console.log(true == 1);
+console.log(0);
